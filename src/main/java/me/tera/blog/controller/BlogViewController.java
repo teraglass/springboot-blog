e7b37c915e1d6d7549ffc5104486a6da92098b87
teraglass <thecodss@gmail.com>
2023-08-24 00:48:09 +0900

    수정, 삭제 추가

--- src/main/java/me/tera/blog/controller/BlogViewController.java
+++ src/main/java/me/tera/blog/controller/BlogViewController.java
@@ -10,28 +10,44 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RequiredArgsConstructor
 @Controller
-@RequestMapping("/articles")
 public class BlogViewController {
 
 	private final BlogService blogService;
 
-	@GetMapping()
-	public String getArticles(Model model){
-		List<ArticleListViewResponse> articles = blogService.findAll().stream()
+	@GetMapping("/articles")
+	public String getArticles(Model model) {
+		List<ArticleListViewResponse> articles = blogService.findAll()
+			.stream()
 			.map(ArticleListViewResponse::new)
 			.toList();
 		model.addAttribute("articles", articles);
+
 		return "articleList";
 	}
 
-	@GetMapping("/{id}")
-	public String getArticle(@PathVariable Long id, Model model){
+	@GetMapping("/articles/{id}")
+	public String getArticle(@PathVariable Long id, Model model) {
 		Article article = blogService.findById(id);
 		model.addAttribute("article", new ArticleViewResponse(article));
+
 		return "article";
+	}
+
+
+	@GetMapping("/new-article")
+	public String newArticle(@RequestParam(required = false) Long id, Model model) {
+		if (id == null) {
+			model.addAttribute("article", new ArticleViewResponse());
+		} else {
+			Article article = blogService.findById(id);
+			model.addAttribute("article", new ArticleViewResponse(article));
+		}
+
+		return "newArticle";
 	}
 }
